0cd0bd12270c55daf0faba2c50f5bdac733bf313
Tim <tkoopman@users.noreply.github.com>
2017-08-24 08:54:29 +1000

    Very DRAFT notepad edited copy of new class

--- CheckPoint/CheckPointCmdlet.cs
+++ CheckPoint/CheckPointCmdlet.cs
@@ -1,14 +1,61 @@
-﻿using CheckPoint.Session;
+using CheckPoint.Session;
 using System.Management.Automation;
 
 namespace CheckPoint
 {
     public abstract class CheckPointCmdlet : Cmdlet
     {
+		// Check Point Web-API Command
+		public abstract string Command { get; }
+		
         /// <summary>
         /// <para type="description">Session object from Open-CheckPointSession</para>
         /// </summary>
         [Parameter(Position = 0, Mandatory = true)]
         public CheckPointSession Session { get; set; }
+		
+		internal string getJSON() { return "{ }"; }
+		
+		protected abstract ProcessRecordResponse(string JSON);
+		
+		protected override void ProcessRecord()
+        {
+            // Debug Output Request
+            string strJson = getJSON();
+            this.WriteDebug($@"JSON Request to {Session.URL}/{Command}
+{strJson}");
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("X-chkp-sid", Session.SID);
+                HttpResponseMessage response = client.PostAsync($"{Session.URL}/{Command}", new StringContent(strJson, Encoding.UTF8, "application/json")).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    strJson = response.Content.ReadAsStringAsync().Result;
+
+                    // Debug Output Request
+                    this.WriteDebug($@"JSON Response
+{strJson}");
+
+                    ProcessRecordResponse(strJSON);
+                }
+                else
+                {
+                    WriteWarning($"Server returned status code: {(int)response.StatusCode} [{response.StatusCode}]");
+                    strJson = response.Content.ReadAsStringAsync().Result;
+                    WriteDebug(strJson);
+                    CheckPointError error = JsonConvert.DeserializeObject<CheckPointError>(strJson);
+                    WriteObject(error);
+                }
+            }
+            catch (Exception e)
+            {
+                while (e.InnerException != null) e = e.InnerException;
+                WriteError(new ErrorRecord(e, e.Message, ErrorCategory.ConnectionError, this));
+            }
+        }
     }
 }
