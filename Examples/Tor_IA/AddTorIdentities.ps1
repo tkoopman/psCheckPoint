41bf2831623beca8173ca2100173a544aa28c5a8
Tim <tkoopman@users.noreply.github.com>
2017-08-27 14:40:41 +1000

    Created psCheckPointIA Module

---
+++ Examples/Tor_IA/AddTorIdentities.ps1
@@ -0,0 +1,37 @@
+#
+# AddTorIdentities.ps1
+#
+
+# Full list of Tor Nodes
+$URL="https://www.dan.me.uk/torlist/"
+# Limit to just exit nodes - Uncomment if you wish
+#$URL="$($URL)?exit"
+
+$Cache="TorNodesCache.csv"
+
+$Download=$false
+if (Test-Path $Cache) {
+	$lastWrite = (get-item $Cache).LastWriteTime
+	$timespan = new-timespan -Seconds 5 # -minutes 30
+
+	if (((get-date) - $lastWrite) -gt $timespan) {
+		$Download=$true
+		Write-Warning "Tor nodes cache older than 30 minutes. Downloading new copy from $($URL)"
+	}
+} else {
+	$Download=$true
+	Write-Warning "Tor nodes cache missing. Downloading new copy from $($URL)"
+}
+
+if ($Download) {
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+	Invoke-WebRequest -Uri $URL -OutFile $Cache
+}
+
+if (Test-Path $Cache) {
+	Import-Csv -Path $Cache -Header "IPAddress" |
+		Add-CheckPointIdentity -SessionTimeout 7200 -NoFetchUserGroups -NoFetchMachineGroups -NoCalculateRoles -NoCertificateValidation -Roles 'Tor' -HostType 'Tor Node' -Machine {"Tor_$($_.IPAddress)"} -BatchSize 100 |
+		Format-Table -Wrap
+} else {
+	Write-Error "Failed to download list."
+}
---
+++ psCheckPointIA/Batch.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using psCheckPoint;
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace psCheckPointIA
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    internal class Batch<RequestType, ResponseType>
+    {
+        public Batch(string gateway, string sharedSecret, string command)
+        {
+            Gateway = gateway;
+            SharedSecret = sharedSecret;
+            Command = command;
+        }
+
+        public string Gateway { get; set; }
+
+        [JsonProperty(PropertyName = "shared-secret")]
+        public string SharedSecret { get; set; }
+
+        public string Command { get; set; }
+
+        [JsonProperty(PropertyName = "requests")]
+        public List<RequestType> Requests { get; set; } = new List<RequestType>(10);
+
+        public void Post(Cmdlet cmdlet)
+        {
+            string URL = $"https://{Gateway}/_IA_API/v1.0/{Command}";
+
+            // Debug Output Request
+            JsonSerializerSettings jss = new JsonSerializerSettings();
+            jss.MaxDepth = 4;
+            string strJson = JsonConvert.SerializeObject(this, jss);
+            cmdlet.WriteDebug($@"JSON Request to {URL}
+{strJson}");
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = client.PostAsync(URL, new StringContent(strJson, Encoding.UTF8, "application/json")).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    strJson = response.Content.ReadAsStringAsync().Result;
+
+                    // Debug Output Request
+                    cmdlet.WriteDebug($@"JSON Response
+{strJson}");
+
+                    Responses<ResponseType> responses = JsonConvert.DeserializeObject<Responses<ResponseType>>(strJson);
+
+                    cmdlet.WriteObject(responses.responses);
+                }
+                else
+                {
+                    cmdlet.WriteWarning($"Server returned status code: {(int)response.StatusCode} [{response.StatusCode}]");
+                    strJson = response.Content.ReadAsStringAsync().Result;
+
+                    // Debug Output Request
+                    cmdlet.WriteDebug($@"JSON Response
+{strJson}");
+
+                    CheckPointError error = JsonConvert.DeserializeObject<CheckPointError>(strJson);
+                    cmdlet.WriteObject(error);
+                }
+            }
+            catch (Exception e)
+            {
+                while (e.InnerException != null) e = e.InnerException;
+                cmdlet.WriteError(new ErrorRecord(e, e.Message, ErrorCategory.ConnectionError, this));
+            }
+        }
+    }
+}
