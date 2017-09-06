f0e055ce5d422bd900143f02ad5325896ea174a7
Tim <tim@koopmail.net>
2017-09-06 20:12:43 +1000

    Improved Export.

--- psCheckPoint/Extra/ExportRule/ConvertToCheckPointHtml.cs
+++ psCheckPoint/Extra/ExportRule/ConvertToCheckPointHtml.cs
@@ -7,12 +7,13 @@ using System.Management.Automation;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace psCheckPoint.Extra.ExportRule
 {
-    [Cmdlet(VerbsData.ConvertTo, "CheckPointHtml")]
+    [Cmdlet(VerbsData.ConvertTo, "CheckPointHtml", DefaultParameterSetName = "ES0")]
     [OutputType(typeof(string))]
-    public class ConvertToCheckPointHtml : Cmdlet
+    public class ConvertToCheckPointHtml : PSCmdlet
     {
         /// <summary>
         /// <para type="description"></para>
@@ -32,12 +33,66 @@ namespace psCheckPoint.Extra.ExportRule
         [Parameter]
         public SwitchParameter Open { get; set; }
 
+        /// <summary>
+        /// <para type="description">Filename to HTML file to use as template.</para>
+        /// <para type="description">{JSON} will be replaced with JSON data.</para>
+        /// <para type="description">Leave blank to use default template.</para>
+        /// </summary>
+        [Parameter]
+        public string Template { get; set; }
+
+        /// <summary>
+        /// <para type="description">Indent the JSON data in the HTML output.</para>
+        /// <para type="description">This will force the use of "Template Literals" which requires an ES6 compatibile browser. (`{JSON}`)</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "ES6")]
+        public SwitchParameter IndentedJson { get; set; }
+
+        /// <summary>
+        /// <para type="description">Escape JSON text before inserting into HTML file.</para>
+        /// <para type="description">Automatically turned on if templated uses double quotes to define location for JSON. ("{JSON}")</para>
+        /// </summary>
+        [Parameter(ParameterSetName = "ES5")]
+        public SwitchParameter EscapeJson { get; set; }
+
         protected override void ProcessRecord()
         {
-            Assembly _assembly = Assembly.GetExecutingAssembly();
-            StreamReader _html = new StreamReader(_assembly.GetManifestResourceStream("psCheckPoint.Extra.ExportRule.ExportCheckPointAccessRule.html"));
-            string html = _html.ReadToEnd().Replace("{JSON}", JsonConvert.SerializeObject(Export));
-            _html.Close();
+            string html = null;
+            if (String.IsNullOrWhiteSpace(Template))
+            {
+                // Load default template
+                Assembly _assembly = Assembly.GetExecutingAssembly();
+                StreamReader _html = new StreamReader(_assembly.GetManifestResourceStream("psCheckPoint.Extra.ExportRule.ExportCheckPointAccessRule.html"));
+                html = _html.ReadToEnd();
+                _html.Close();
+            }
+            else
+            {
+                // Load template from file specified
+                html = System.IO.File.ReadAllText(Template);
+            }
+
+            if (String.IsNullOrWhiteSpace(html))
+            {
+                throw new CmdletInvocationException("Unable to load HTML template.");
+            }
+
+            string json = JsonConvert.SerializeObject(Export, (IndentedJson.IsPresent) ? Formatting.Indented : Formatting.None);
+
+            // See if we need to force ES6
+            if (this.ParameterSetName.Equals("ES6"))
+            {
+                if (html.Contains("\"{JSON}\""))
+                {
+                    html = html.Replace("\"{JSON}\"", "`{JSON}`");
+                }
+            }
+            else if (EscapeJson.IsPresent || html.Contains("\"{JSON}\""))
+            {
+                json = HttpUtility.JavaScriptStringEncode(json);
+            }
+
+            html = html.Replace("{JSON}", json);
 
             if (Open.IsPresent && Out == null)
             {
--- psCheckPoint/Extra/ExportRule/ExportCheckPointAccessRule.cs
+++ psCheckPoint/Extra/ExportRule/ExportCheckPointAccessRule.cs
@@ -27,7 +27,7 @@ namespace psCheckPoint.Extra.ExportRule
         /// <para type="description"></para>
         /// </summary>
         [Parameter(Position = 1, Mandatory = true, ValueFromPipeline = true)]
-        public CheckPointAccessRule Rule { get; set; }
+        public PSObject Object { get; set; }
 
         /// <summary>
         /// <para type="description"></para>
@@ -44,86 +44,129 @@ namespace psCheckPoint.Extra.ExportRule
 
         protected override void ProcessRecord()
         {
-            export.Rules.Add(Rule);
+            Process(Object.BaseObject, 0);
         }
 
         protected override void EndProcessing()
         {
-            foreach (CheckPointAccessRule rule in export.Rules)
-            {
-                foreach (CheckPointObject obj in rule.Source)
-                {
-                    Process(obj, 0);
-                }
-                foreach (CheckPointObject obj in rule.Destination)
-                {
-                    Process(obj, 0);
-                }
-            }
             WriteObject(export);
+        }
+
+        private void Process(object obj, int CurrentDepth)
+        {
+            if (obj is CheckPointObject)
+            {
+                Process(obj as CheckPointObject, CurrentDepth);
+            }
+            else
+            {
+                throw new CmdletInvocationException($"Invalid object type: {obj.GetType()}");
+            }
         }
 
         private void Process(CheckPointObject obj, int CurrentDepth)
         {
             switch (obj.Type)
             {
+                case "access-rule":
+                    {
+                        if (export.Rules.Contains(obj)) { return; }
+                        CheckPointAccessRule rule = ProcessCheckPointObject<CheckPointAccessRule>(obj, CurrentDepth, "Get-CheckPointAccessRule", typeof(GetCheckPointAccessRule));
+                        if (rule != null) { Process(rule, CurrentDepth); }
+                        break;
+                    }
                 case "host":
                     {
-                        if (!export.Hosts.Contains(obj))
-                        {
-                            using (PowerShell PSI = PowerShell.Create())
-                            {
-                                PSI.AddCommand(new CmdletInfo("Get-CheckPointHost", typeof(GetCheckPointHost)));
-                                PSI.AddParameter("Session", Session);
-                                PSI.AddParameter("UID", obj.UID);
-
-                                Collection<CheckPointHost> hosts = PSI.Invoke<CheckPointHost>();
-                                foreach (CheckPointHost host in hosts)
-                                {
-                                    export.Hosts.Add(host);
-                                }
-                            }
-                        }
+                        if (export.Hosts.Contains(obj)) { return; }
+                        CheckPointHost host = ProcessCheckPointObject<CheckPointHost>(obj, CurrentDepth, "Get-CheckPointHost", typeof(GetCheckPointHost));
+                        if (host != null) { Process(host, CurrentDepth); }
                         break;
                     }
                 case "group":
                     {
-                        if (!export.Groups.Contains(obj))
-                        {
-                            using (PowerShell PSI = PowerShell.Create())
-                            {
-                                PSI.AddCommand(new CmdletInfo("Get-CheckPointGroup", typeof(GetCheckPointGroup)));
-                                PSI.AddParameter("Session", Session);
-                                PSI.AddParameter("UID", obj.UID);
-
-                                Collection<CheckPointGroup> groups = PSI.Invoke<CheckPointGroup>();
-                                foreach (CheckPointGroup group in groups)
-                                {
-                                    export.Groups.Add(group);
-                                    if (CurrentDepth < Depth)
-                                    {
-                                        foreach (CheckPointObject member in group.Members)
-                                        {
-                                            Process(member, ++CurrentDepth);
-                                        }
-                                    }
-                                    else
-                                    {
-                                        WriteVerbose("Depth limit reached!");
-                                    }
-                                }
-                            }
-                        }
+                        if (export.Groups.Contains(obj)) { return; }
+                        CheckPointGroup group = ProcessCheckPointObject<CheckPointGroup>(obj, CurrentDepth, "Get-CheckPointGroup", typeof(GetCheckPointGroup));
+                        if (group != null) { Process(group, CurrentDepth); }
                         break;
                     }
                 default:
                     {
                         if (!export.Other.Contains(obj))
                         {
+                            WriteVerbose($"Exporting* {obj.Type}: {obj.Name}");
                             export.Other.Add(obj);
                         }
                         break;
                     }
+            }
+        }
+
+        private void Process(CheckPointAccessRule rule, int CurrentDepth)
+        {
+            if (export.Rules.Contains(rule)) { return; }
+
+            string name = (String.IsNullOrWhiteSpace(rule.Name)) ? rule.UID : rule.Name;
+            WriteVerbose($"Exporting access rule: {name}");
+
+            export.Rules.Add(rule);
+
+            if (CurrentDepth < Depth)
+            {
+                foreach (CheckPointObject newObj in rule.Source)
+                {
+                    Process(newObj, CurrentDepth + 1);
+                }
+                foreach (CheckPointObject newObj in rule.Destination)
+                {
+                    Process(newObj, CurrentDepth + 1);
+                }
+            }
+            else
+            {
+                WriteVerbose("Depth limit reached!");
+            }
+        }
+
+        private void Process(CheckPointGroup group, int CurrentDepth)
+        {
+            if (export.Groups.Contains(group)) { return; }
+
+            WriteVerbose($"Exporting group: {group.Name}");
+            export.Groups.Add(group);
+
+            if (CurrentDepth < Depth)
+            {
+                foreach (CheckPointObject member in group.Members)
+                {
+                    Process(member, CurrentDepth + 1);
+                }
+            }
+            else
+            {
+                WriteVerbose("Depth limit reached!");
+            }
+        }
+
+        private void Process(CheckPointHost host, int CurrentDepth)
+        {
+            if (export.Hosts.Contains(host)) { return; }
+
+            WriteVerbose($"Exporting host: {host.Name}");
+            export.Hosts.Add(host);
+        }
+
+        private T ProcessCheckPointObject<T>(dynamic obj, int CurrentDepth, string psCmdletName, Type psCmdlet)
+        {
+            if (obj is T) { return (T)obj; }
+
+            using (PowerShell PSI = PowerShell.Create())
+            {
+                PSI.AddCommand(new CmdletInfo(psCmdletName, psCmdlet));
+                PSI.AddParameter("Session", Session);
+                PSI.AddParameter("UID", obj.UID);
+
+                Collection<T> results = PSI.Invoke<T>();
+                return results.First();
             }
         }
     }
