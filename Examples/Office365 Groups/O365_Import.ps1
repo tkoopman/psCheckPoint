4bd84a16c8a9d77f9b70fc61b649de9f15dd5507
Tim <tkoopman@users.noreply.github.com>
2017-09-13 14:25:37 +1000

    O365 Import Example
    * O365 Import script added based off of Hugo's script
    * Added ability to add/remove group members to Set-CheckPointGroup command
    * Updated to v0.3.3

---
+++ Examples/Office365 Groups/O365_Import.ps1
@@ -0,0 +1,219 @@
+#
+# Download Office365/Azure network Details
+#
+# (C) 2017, Hugo van der Kooij
+#
+# Don't forget to run `Install-Module psCheckPoint` (as administrator) once!
+#
+# WARNING: This script will put a significant load on your SmartCenter!
+#
+param(
+	[switch]$NoIPv4,
+	[switch]$NoIPv6,
+	[switch]$PrintURLs,
+	[switch]$Publish,
+	[switch]$IgnoreWarnings
+)
+
+# Download Microsoft Cloud IP Ranges and Names into Object
+$O365IPAddresses = "https://support.content.office.net/en-us/static/O365IPAddresses.xml"
+[XML]$O365 = Invoke-WebRequest -Uri $O365IPAddresses -DisableKeepAlive
+
+$Updated = $O365.products.updated
+$Comments = "Microsoft Office365 added $Updated"
+$MSO365 = "Microsoft_Office365"
+$MS = "Microsoft"
+
+# Login to Check Point API to get Session ID
+Write-Verbose " *** Log in to Check Point Smart Center API *** "
+$Session = Open-CheckPointSession -SessionName $MSO365 -SessionComments "Microsoft Office365 Filler" -SessionTimeOut 1800 -NoCertificateValidation
+if (-not $Session -or $Session.Code) {
+	# Failed login
+	Write-Error "Failed to login to SmartConsole. $Session"
+	exit
+}
+
+$Group = Get-CheckPointGroup -Session $Session -Name $MSO365 -Verbose:$false
+if ($Group.Code) {
+	# Main group does not exist. Create it
+	Write-Verbose "Creating main group $MSO365"
+	$Group = New-CheckPointGroup -Session $Session -Name $MSO365 -Tag $MSO365 -Color Red -Comments "$Comments"
+	if ($Group.Code) {
+		Write-Error "Failed to create group $MSO365. $Group"
+		exit
+	}
+}
+
+# Will keep track of hosts removed from groups.
+#After processing we can check these to see if any should be deleted.
+$Removed = New-Object System.Collections.ArrayList
+
+ForEach ($Product in $O365.products.product) {
+	$GroupName = $MSO365 + "_" + $Product.Name
+
+	# Check if group exists and get existing members
+	$Group = Get-CheckPointGroup -Session $Session -Name $GroupName -Verbose:$false
+
+	if ($Group.code) {
+		#Group not found
+		Write-Verbose "Creating product group $GroupName"
+		$Group = New-CheckPointGroup -Session $Session -Name $GroupName -Tag $MSO365 -Groups $MSO365 -Color Red -Verbose:$false
+		$Existing = @()
+		if ($Group.Code) {
+			Write-Error "Failed to create group $GroupName. $Group"
+			exit
+		}
+	} else {
+		$Existing = $Group.Members.Name
+		$Count = $Existing.Count
+		if ($Count -eq 0) {
+			$Existing = @()
+		}
+		Write-Verbose "Group $GroupName already exists with $Count members"
+	}
+
+	$MSList = New-Object System.Collections.ArrayList
+	ForEach ($AddressList in $Product.addresslist) {
+		$Type = $AddressList.type
+		if (($Type -eq "IPv4" -and $NoIPv4.IsPresent) -or ($Type -eq "IPv6" -and $NoIPv6.IsPresent)) {
+			Write-Verbose "Skipping $Type for $GroupName"
+		} else {
+			ForEach ($Address in $AddressList.address) {
+				ForEach ($Entry in $Address) {
+					If ($Type -eq "IPv4" ) {
+						$Network = $Entry.split("/")[0]
+						$MaskLength = $Entry.split("/")[1]
+						If ($MaskLength -eq 32) {
+							$i = $MSList.Add("$($MSO365)_$($Network)")
+						} else {
+							$i = $MSList.Add("$($MSO365)_$($Entry)")
+						}
+					} ElseIf ($Type -eq "IPv6") {
+						$Network = $Entry.split("/")[0]
+						$MaskLength = $Entry.split("/")[1]
+						If ($Entry -notlike "*/") {
+							Write-Verbose "Look $Entry"
+							$MaskLength = 128
+						}
+						If ($MaskLength -eq 128) {
+							$i = $MSList.Add("$($MSO365)_$($Network)")
+						} else {
+							$i = $MSList.Add("$($MSO365)_$($Entry)")
+						}
+					} ElseIf ($Type -eq "URL") {
+						if ($PrintURLs.IsPresent) {
+							Write-Host " Hostname ($GroupName) : $Entry"
+						}
+					}
+				}
+			}
+		}
+	}
+
+	$MSList = $MSList | Select -Unique
+	if ($MSList.Count -eq 0) {
+		$MSList = @()
+	}
+
+	$Diff = Compare-Object -ReferenceObject $MSList -DifferenceObject $Existing -IncludeEqual -Verbose:$false
+	$ToAdd = New-Object System.Collections.ArrayList
+	$ToRemove = New-Object System.Collections.ArrayList
+	ForEach($Entry in $Diff) {
+		$ObjName = $Entry.InputObject
+		$Split = $Entry.InputObject.split("_")
+		$ObjIP = $Split[-1]
+		$IsNetwork = ($ObjIP -like "*/*")
+		
+		switch ($Entry.SideIndicator)
+		{
+			"<=" {
+				# New entry. Add to group
+				# Check if object exists already
+				if ($IsNetwork) {
+					$Obj = Get-CheckPointNetwork -Session $Session -Name $ObjName -Verbose:$false
+				} else {
+					$Obj = Get-CheckPointHost -Session $Session -Name $ObjName -Verbose:$false
+				}
+				if ($Obj.code) {
+					# Create New Object
+					if ($IsNetwork) {
+						$Network = $ObjIP.split("/")[0]
+						$MaskLength = $ObjIP.split("/")[1]
+						Write-Verbose "Creating network $ObjName in $GroupName"
+						$Obj = New-CheckPointNetwork -Session $Session -Name $ObjName -Subnet $Network -MaskLength $MaskLength -Groups $GroupName -Tags $MSO365 -Comments "$Comments" -IgnoreWarnings:$IgnoreWarnings.IsPresent -Verbose:$false
+						if ($Obj.Code) {
+							Write-Warning "Failed to create network $ObjName in $GroupName. $Obj"
+						}
+					} else {
+						Write-Verbose "Creating host $ObjName in $GroupName"
+						$Obj = New-CheckPointHost -Session $Session -Name $ObjName -ipAddress $ObjIP -Groups $GroupName -Tags $MSO365 -Comments "$Comments" -IgnoreWarnings:$IgnoreWarnings.IsPresent -Verbose:$false
+						if ($Obj.Code) {
+							Write-Warning "Failed to create host $ObjName in $GroupName. $Obj"
+						}
+					}
+				} else {
+					# Add existing object to group
+					Write-Verbose "Adding $ObjName to $GroupName"
+					$i = $ToAdd.Add($ObjName)
+				}
+			}
+			"=>" {
+				# No longer required. Remove from group
+				Write-Verbose "Removing $ObjName from $GroupName"
+				$i = $ToRemove.Add($ObjName)
+				$i = $Removed.Add($ObjName)
+			}
+			"==" {
+				# Already in group. No change needed
+				#Write-Verbose "Leaving $ObjName in $GroupName"
+			}
+		}
+	}
+	if ($ToAdd.Count -gt 0) {
+		$Obj = Set-CheckPointGroup -Session $Session -Name $GroupName -AddMembers $ToAdd -Verbose:$false
+		if ($Obj.Code) {
+			Write-Warning "Failed to add new group members to $GroupName. $Obj"
+		}
+	}
+	if ($ToRemove.Count -gt 0) {
+		$Obj = Set-CheckPointGroup -Session $Session -Name $GroupName -RemoveMembers $ToRemove -Verbose:$false
+		if ($Obj.Code) {
+			Write-Warning "Failed to remove old group members from $GroupName. $Obj"
+		}
+	}
+}
+
+ForEach($Name in $Removed) {
+	$used = Get-CheckPointWhereUsed -Session $Session -Name $Name
+	if ($used.UsedDirectly.Total -eq 0) {
+		Write-Verbose "Deleting $Name"
+		if ($Name -like "*/*") {
+			$Msg = Remove-CheckPointNetwork -Session $Session -Name $Name
+			if ($Msg.Code) {
+				Write-Warning "Failed to remove unused network $Name. $Msg"
+			}
+		} else {
+			$Msg = Remove-CheckPointHost -Session $Session -Name $Name
+			if ($Msg.Code) {
+				Write-Warning "Failed to remove unused host $Name. $Msg"
+			}
+		}
+	}
+}
+
+$Stats = Get-CheckPointSession -Session $Session -UID $Session.UID
+if ($Stats.Changes -eq 0) {
+	Write-Host "No changes made. Closing session."
+	Reset-CheckPointSession -Session $Session -Verbose:$false
+	Close-CheckPointSession -Session $Session -Verbose:$false
+} elseif ($Publish.IsPresent) {
+	# Publish Changes
+	Write-Host "Publishing $($Stats.Changes) changes."
+	Publish-CheckPointSession -Session $Session -Verbose:$false
+	Close-CheckPointSession -Session $Session -Verbose:$false
+} else {
+	# Logout from Check Point API
+	Write-Host "View $($Stats.Changes) changes in SmartConsole to publish."
+	Close-CheckPointSession -Session $Session -ContinueSessionInSmartconsole -Verbose:$false
+}
+# DONE!
