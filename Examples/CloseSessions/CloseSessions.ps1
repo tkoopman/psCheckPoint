f97ec8f7cf92e6f860986d4d3dbd24c7f3b6944c
Tim <tkoopman@users.noreply.github.com>
2018-03-30 17:46:03 +1000

    Updated

--- Examples/CloseSessions/CloseSessions.ps1
+++ Examples/CloseSessions/CloseSessions.ps1
@@ -1,4 +1,4 @@
 #
 # CloseSessions.ps1
 #
-(Get-CheckPointSessions).Sessions | where {$_.ExpiredSession -and $_.Changes -eq 0} | Reset-CheckPointSession
+(Get-CheckPointSessions).Sessions | where {$_.IsExpiredSession -and $_.Changes -eq 0} | Reset-CheckPointSession
