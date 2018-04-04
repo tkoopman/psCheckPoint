14fd8cddf0675613a4f00a811c7e84c03257cf79
Tim Koopman <tkoopman@users.noreply.github.com>
2018-04-05 08:52:25 +1000

    Add task tests

---
+++ Pester.Tests/Tests/Task.Tests.ps1
@@ -0,0 +1,19 @@
+﻿Describe "Basic.Tasks" {
+	Context "Invoke-CheckPointScript" {
+		$task = Invoke-CheckPointScript -ScriptName "Get Configuration" -Script "clish -c 'show hostname'" -Targets mgmt -Session $Session
+
+		It "Confirm Invoke-CheckPointScript returns task" {
+			$task["mgmt"] | Should BeOfType System.String
+		}
+
+		$t = $task["mgmt"] | Wait-CheckPointTask -Session $Session
+
+		It "Confirm task completes" {
+			$t.Status | Should be "Succeeded"
+		}
+
+		It "Confirm task includes script response" {
+			$t.TaskDetails.ResponseMessage | Should -Match "mgmt"
+		}
+	}
+}
