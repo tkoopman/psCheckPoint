07b04ad8872f95eec8872c492f9c6c041718279e
Tim <tkoopman@users.noreply.github.com>
2018-05-13 09:41:01 +1000

    Updated examples

--- Examples/SaveConfigurations/SaveConfigs.ps1
+++ Examples/SaveConfigurations/SaveConfigs.ps1
@@ -1,7 +1,9 @@
 #
 # SaveConfigs.ps1
 #
-$(Get-CheckPointSimpleGateways).Objects | 
+# Make sure you already have an open session to management then run this to download configs from each simple gateway
+#
+Get-CheckPointSimpleGateways -All |
   ForEach-Object {
 	  $(Invoke-CheckPointScript -ScriptName "Get Configuration" -Script "clish -c 'Show Configuration'" -Targets $_.Name | Wait-CheckPointTask).TaskDetails.ResponseMessage | Out-File "$($_.Name).conf"
   }
